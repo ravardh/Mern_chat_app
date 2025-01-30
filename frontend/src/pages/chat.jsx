95b248e9ee620283584552b23196cf44e97cbf15
Raj Vardhan <rajvardaan130@gmail.com>
2025-01-30 17:53:08 +0530

    removed socket and niow code running properly

--- frontend/src/pages/chat.jsx
+++ frontend/src/pages/chat.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "../lib/axios";
 import dummy from "../assests/dummyPP.png";
-import { socket } from "../lib/socket";
+
 
 const ChatPage = () => {
   const [selectedChat, setSelectedChat] = useState(null);
